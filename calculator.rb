bac0dd6471f1105c1f2babd32c3ab09a3e178db4
Dan Garland <dan@dangarland.co.uk>
2015-09-26 20:47:27 +0100

    Making the window not resizble

--- calculator.rb
+++ calculator.rb
@@ -1,21 +1,23 @@
-Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
-  stack :margin => 20 do
-    @output = edit_line
-    
+Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240, :resizable => false do
+  background "#333", :curve => 10, :margin => 25
+
+  stack :margin => 40 do
+    @output = edit_line :width => 160
+
     flow do
-      %w(0 1 2 3 4 5 6 7 8 9 + / * -).each do |op|      
-        button op do         
+      %w(0 1 2 3 4 5 6 7 8 9 + / * -).each do |op|
+        button op do
           append op
         end
       end
-      
+
       button "=" do
         eval_expression
       end
     end
-    
+
   end
-  
+
   # Stick a string on the end of our input
   #
   def append(s)
@@ -25,12 +27,12 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
     @input += s
     @output.text = @input
   end
-  
+
   # Evaluate the input we've got so far
   #
   def eval_expression
     @input = eval(@input).to_s
     @output.text = @input
   end
-  
+
 end
