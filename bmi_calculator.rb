7614887373dfde32a2ed0f269ebe9b45690d7945
Finn Francis <finn_francis@yahoo.com>
2016-05-16 22:13:34 +0100

    Added the BMI calculator

--- bmi_calculator.rb
+++ bmi_calculator.rb
@@ -1,52 +1,58 @@
-Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240, :resizable => false do
-  background "#333", :curve => 10, :margin => 25
+Shoes.app :title => "My Amazing BMI Calculator", :width => 240, :height => 300, :resizable => false do
+  background "#fff", :curve => 80, :margin => 10
 
   stack :margin => 40 do
-    @output = edit_line :width => 160
-    @input_height = edit_line :width => 50
-    @input_weight = edit_line :width => 50
+    subtitle "    BMI"
+    para "           height"
+    para "\n          weight"
+
+    @output = para
+
+    @input_height = edit_line :width => 120
+    @input_weight = edit_line :width => 120
+    @input_height.move(20, 80)
+    @input_weight.move(20, 120)
+
+    @output.move(100, 250)
 
     @height = true
 
     puts "hello"
 
     flow do
-      %w(0 1 2 3 4 5 6 7 8 9).each do |op|
-        button op do
-          append op
-        end
-      end
+      # %w(0 1 2 3 4 5 6 7 8 9).each do |op|
+      #   button op do
+      #     append op
+      #   end
+      # end
 
-      button "height" do
-
-      end
-
-      button "=" do
+      button "        =        " do
         eval_expression
       end
+
     end
 
   end
 
   # Stick a string on the end of our input
   #
-  def append(s)
-    if @input.nil?
-      @input = ""
-    end
-    @input += s
-    @output.text = @input
-  end
+  # def append(s)
+  #   if @input.nil?
+  #     @input = ""
+  #   end
+  #   @input += s
+  #   @output.text = @input
+  # end
 
   def calc_bmi
-    return @input_weight / (@input_height ** 2)
+    return @input_weight.text.to_f / (@input_height.text.to_f ** 2)
   end
 
   # Evaluate the input we've got so far
   #
   def eval_expression
     # @input = eval(@input).to_s
-    @output.text = calc_bmi
+    @output.text = calc_bmi.to_s
   end
 
 end
