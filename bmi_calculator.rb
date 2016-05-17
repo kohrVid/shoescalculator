0c81d16295839f3c7e644990175e1343db3d5f5d
kohrVid <kohrVid@zoho.com>
2016-05-17 20:22:41 +0100

    Fixing issues with BMI calculator layout

--- bmi_calculator.rb
+++ bmi_calculator.rb
@@ -3,30 +3,22 @@ Shoes.app :title => "My Amazing BMI Calculator", :width => 240, :height => 300, 
 
   stack :margin => 40 do
     subtitle "    BMI"
-    para "           height"
-    para "\n          weight"
+    para "       height (m)"
+    para "\n       weight (kg)"
 
     @output = para
 
     @input_height = edit_line :width => 120
     @input_weight = edit_line :width => 120
     @input_height.move(20, 80)
-    @input_weight.move(20, 120)
+    @input_weight.move(20, 137)
 
-    @output.move(100, 250)
+    @output.move(25, 253)
 
     @height = true
 
-    puts "hello"
-
-    flow do
-      # %w(0 1 2 3 4 5 6 7 8 9).each do |op|
-      #   button op do
-      #     append op
-      #   end
-      # end
-
-      button "        =        " do
+    flow :margin => 40 do
+      button "Calculate BMI", :width => "155%" do
         eval_expression
       end
 
@@ -34,24 +26,12 @@ Shoes.app :title => "My Amazing BMI Calculator", :width => 240, :height => 300, 
 
   end
 
-  # Stick a string on the end of our input
-  #
-  # def append(s)
-  #   if @input.nil?
-  #     @input = ""
-  #   end
-  #   @input += s
-  #   @output.text = @input
-  # end
 
   def calc_bmi
     return @input_weight.text.to_f / (@input_height.text.to_f ** 2)
   end
 
-  # Evaluate the input we've got so far
-  #
   def eval_expression
-    # @input = eval(@input).to_s
     @output.text = calc_bmi.to_s
   end
 
